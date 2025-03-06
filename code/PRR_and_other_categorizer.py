42d5e50d24f7f3fa22cd06e84c50c8c904515ae4
arefks <arefks@gmail.com>
2025-03-06 10:43:17 +0100

    added figures for prr, fitters non fitters, added powerpoint figs, calculauted percentages of different trajectories of recovery

--- code/PRR_and_other_categorizer.py
+++ code/PRR_and_other_categorizer.py
@@ -31,26 +31,38 @@ os.makedirs(pppath, exist_ok=True)
 
 # Read the CSV file into a Pandas DataFrame
 df = pd.read_csv(input_file_path)
-
+df = df[df["fixed_type"]=="bad"]
 # Filter out subjects with less than 3 records and NaN values
 subject_counts = df["record_id"].value_counts()
 neglected_subjects = subject_counts[subject_counts < 3].index.tolist()
 df_filtered = df[~df["record_id"].isin(neglected_subjects)]
 df_filtered.reset_index(drop=True, inplace=True)
 
+
 # Normalize each score to a 0-1 range and compute the average
 df_filtered["MOTOR_SCORE"] = ((df_filtered["lex"] / 86) + (df_filtered["uex"] / 126)) / 2
+#df_filtered["MOTOR_SCORE"] = df_filtered["lex"]
+
+
+# Compute z-scores for 'lex' and 'uex'
+#df_filtered["lex_z"] = (df_filtered["lex"] - df_filtered["lex"].mean()) / df_filtered["lex"].std()
+#df_filtered["uex_z"] = (df_filtered["uex"] - df_filtered["uex"].mean()) / df_filtered["uex"].std()
+
+# Average the z-scores to get MOTOR_SCORE
+#df_filtered["MOTOR_SCORE"] = (df_filtered["lex_z"] + df_filtered["uex_z"]) / 2
+
+
 
 # Plot motor score over time
 motor_over_time = df_filtered.groupby('tp')['MOTOR_SCORE'].mean().reset_index()
-plt.figure(figsize=(6, 4))
+plt.figure(figsize=(18/2.53, 10/2.53))
 plt.plot(motor_over_time['tp'], motor_over_time['MOTOR_SCORE'], marker='o')
 plt.xlabel('Time Point')
 plt.ylabel('Average Motor Score')
 plt.title('Motor Score Over Time')
 plt.tight_layout()
 plt.show()
-#%%
+#%
 # Pivot the DataFrame for time-dependent variables only.
 constant_cols = ['record_id', 'fixed_type', 'recovery_type']
 df_constants = df_filtered[constant_cols].drop_duplicates()
@@ -62,13 +74,14 @@ df_wide = pd.merge(df_time, df_constants, on='record_id')
 
 # Calculate INITIAL_IMPAIRMENT and CHANGE_OBSERVED
 df_wide['INITIAL_IMPAIRMENT'] = 1 - df_wide['MOTOR_SCORE_tp0']
-df_wide['CHANGE_OBSERVED'] = df_wide['MOTOR_SCORE_tp2'] - df_wide['INITIAL_IMPAIRMENT'] 
-error = 0.9
-df_wide['CHANGE_PREDICTED'] = -0.7 * df_wide['INITIAL_IMPAIRMENT'] + error
+df_wide['CHANGE_OBSERVED'] =  -df_wide['MOTOR_SCORE_tp0'] +df_wide['MOTOR_SCORE_tp2']  
+error = -0.8
+df_wide['CHANGE_PREDICTED'] = 0.7 * df_wide['INITIAL_IMPAIRMENT'] + error
 #plt.scatter(df_wide['INITIAL_IMPAIRMENT'], df_wide['CHANGE_OBSERVED'], marker='o')
 #%
 # Initialize 'LINE_CLUSTER' to 'HRR'
 df_wide['LINE_CLUSTER'] = "HRR"
+
 
 # Initial linear regression on the whole dataset (if needed)
 x_initial = df_wide['INITIAL_IMPAIRMENT']
@@ -76,17 +89,14 @@ y_initial = df_wide['CHANGE_OBSERVED']
 slope_initial, intercept_initial, r_value_initial, p_value_initial, std_err_initial = linregress(x_initial, y_initial)
 regression_line_initial = slope_initial * x_initial + intercept_initial
 residuals_initial = y_initial - regression_line_initial
-#plt.scatter(df_wide['INITIAL_IMPAIRMENT'], df_wide['CHANGE_OBSERVED'], marker='o')
-#plt.plot(x_initial, regression_line_initial)
-#plt.plot(df_wide['INITIAL_IMPAIRMENT'], df_wide['CHANGE_PREDICTED'] )
 
 
-#%%
+#%
 # Define number of iterations
-R = 3 # You can adjust this value
+R = 10 # You can adjust this value
 
 # Loop for iterative refinement
-for cc in range(R):
+for cc in range(R-1):
     # Perform linear regression on HRR points
     HRR_points = df_wide[df_wide["LINE_CLUSTER"] == "HRR"]
     x_HRR = HRR_points['INITIAL_IMPAIRMENT']
@@ -98,14 +108,12 @@ for cc in range(R):
     else:
         slope, intercept, r_value, p_value, std_err = linregress(x_HRR, y_HRR)
     
-    if cc == 0:
-        intercept=0.8
-    else:
-        intercept = round(intercept, 2)
+
+    intercept = round(intercept, 2)
         
     print(intercept)
     # Use a consistent PRR slope of -0.7
-    prr_slope =-0.7
+    prr_slope =0.7
     
     # Calculate Euclidean distances for all points
     dist_to_regression = euclidean_distance(df_wide['INITIAL_IMPAIRMENT'], 
@@ -157,13 +165,13 @@ for cc in range(R):
         s = 50
         linewidth = 1
     else:
-        plt.figure(figsize=(4.5 * cm, 4.5 * cm), dpi=300)
+        plt.figure(figsize=(9 * cm, 9 * cm), dpi=300)
         s = 15
         linewidth = 0.5
 
     # Scatterplot with hue indicating the cluster; outliers are colored red
     sns.scatterplot(x='INITIAL_IMPAIRMENT', y='CHANGE_OBSERVED', hue='LINE_CLUSTER', data=df_wide,
-                    palette={'HRR': "g", 'PRR': "b", 'outlier': "red"}, marker='o', s=s, alpha=0.7)
+                    palette={'HRR': "g", 'PRR': "b", 'outlier': "red"}, marker='o', s=25, alpha=0.7)
     
     # Define an x-range for plotting the lines
     x_range = np.linspace(df_wide['INITIAL_IMPAIRMENT'].min(), df_wide['INITIAL_IMPAIRMENT'].max(), 100)
@@ -177,19 +185,22 @@ for cc in range(R):
              label=f'PRR Line: y = {prr_slope}x + {current_intercept}')
     
     # Set labels and styling
-    plt.xlabel('INITIAL_IMPAIRMENT', fontsize=8)
-    plt.ylabel('CHANGE_OBSERVED', fontsize=8)
+    plt.xlabel('INITIAL_IMPAIRMENT (Max - Acute)', fontsize=8)
+    plt.ylabel('CHANGE_OBSERVED (Chronic - Acute)', fontsize=8)
     plt.xticks(fontsize=8)
     plt.yticks(fontsize=8)
-    plt.ylim([-2, 2])
-    plt.xlim([-0.5, 1])
+    #plt.ylim([-2, 2])
+    #plt.xlim([-0.5, 1])
     
     ax = plt.gca()
     ax.spines['right'].set_visible(False)
     ax.spines['top'].set_visible(False)
-    plt.legend(frameon=False, fontsize=3)
-    plt.tight_layout()
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left', frameon=False, fontsize=10, markerscale=0.5)
+    plt.tight_layout()  # Adjust layout to accommodate the legend outside
+    plt.savefig(os.path.join(pppath, f"{cc}_ppr_rule.png"), dpi=300, bbox_inches='tight')
+    plt.savefig(os.path.join(pppath, f"{cc}_ppr_rule.svg"), bbox_inches='tight')
     plt.show()
+    
     
     # In final iteration, calculate goodness of fit and save outputs
     if cc == (R - 1):
@@ -216,5 +227,33 @@ for cc in range(R):
         # Save R-squared values to a CSV file
         r_squared_values = {"HRR": r_squared_HRR, "PRR": r_squared_PRR}
         r_squared_df = pd.DataFrame(list(r_squared_values.items()), columns=['Cluster', 'R_squared'])
-        r_squared_df.to_csv(os.path.join(outfile_path, 'r_squared_values.csv'), index=False)
+        r_squared_df.to_csv(os.path.join(outfile_path, 'r_squared_values_prr_fitters_nonfitters.csv'), index=False)
         print("\nGoodness of Fit (R-squared) values saved to 'r_squared_values.csv'.")
+
+#%%
+
+plt.scatter(df_wide['INITIAL_IMPAIRMENT'], df_wide['CHANGE_OBSERVED'], marker='o')
+#plt.plot(x_initial, regression_line_initial)
+plt.scatter(df_wide['INITIAL_IMPAIRMENT'], df_wide['CHANGE_PREDICTED'] )
+#%%
+# Extract the data from the DataFrame
+x = df_wide['CHANGE_OBSERVED']
+y = df_wide['CHANGE_PREDICTED']
+
+# Create the scatter plot
+plt.scatter(x, y, label='Data Points')
+
+# Fit a linear regression line (first degree polynomial)
+m, b = np.polyfit(x, y, 1)
+
+# Plot the fitted line
+plt.plot(x, m * x + b, color='red', label='Fitted Line',marker="o",alpha=0.5)
+
+# Add labels and title
+plt.xlabel('Observed Change')
+plt.ylabel('Predicted Change')
+plt.title('Observed vs. Predicted Change with Fitted Line')
+plt.legend()
+
+# Display the plot
+plt.show()
